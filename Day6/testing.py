c1da4ed44bbcc8e862c55143324fae3eb18789f0
henoktilahun <henok.tilahun1996@gmail.com>
2022-03-29 09:11:06 -0400

    day6 exercise

---
+++ Day6/testing.py
@@ -0,0 +1,13 @@
+print("Hello")
+num_char = len("hello")
+print(num_char)
+
+#this is done because of built in functions
+#but what if we want to defind our own functions
+
+def my_function():
+    print("hello")
+    print("bye")
+
+#fuctions can be defind but will need to be called for it to run
+my_function()
