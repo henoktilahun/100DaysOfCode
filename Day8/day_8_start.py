a5ccdbbba0ab02ed5ca4b149a1904119423315f7
henoktilahun <henok.tilahun1996@gmail.com>
2022-05-02 13:41:03 -0400

    Day 8 exercises

---
+++ Day8/day_8_start.py
@@ -0,0 +1,21 @@
+# def greet():
+#     print("first print statement")
+#     print("Second print statement")
+#     print("third print statement")
+
+# greet()
+
+#function that allows for input
+
+# def greet_withname(name):
+#     print(f"Hello {name}")
+#     print(f"What's up {name}")
+
+# greet_withname("Henok")
+
+#functions with more than 1 input
+def greet_with(name, location):
+    print (f"Hello {name}")
+    print (f"What is it like in {location}")
+
+greet_with(name = "Henok",location = "Boston")
