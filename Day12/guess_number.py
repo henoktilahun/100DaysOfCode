2fdd693954aa0a4692d3f02ba3575527458aee80
henoktilahun <henok.tilahun1996@gmail.com>
2023-02-15 07:58:09 -0500

    correct logic

--- Day12/guess_number.py
+++ Day12/guess_number.py
@@ -1,32 +1,40 @@
-#Number Guessing Game Objectives:
-
-# Allow the player to submit a guess for a number between 1 and 100.
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
-# If they got the answer correct, show the actual answer to the player.
-# Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player. 
-# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
-
-
 from art import logo
 import random
 
 
 print(logo)
 
+def game_level(difficulty_level):
+    if difficulty_level == "easy":
+        turns = 10
+    elif difficulty_level == "hard":
+        turns = 5
+    
+    return turns
 
-turns = 0
 
-while turns < 5:
-    print(f"Turn: {turns}")
-    player_guess = int(input("Please guess a number between 1 and 100: "))
+def play_game():
     random_number = random.randint(1,101)
-    if player_guess < 1:
-        print("Too low")
-    elif player_guess > 100:
-        print("Too High")
-    elif player_guess == random_number:
-        print(f"Correct! number is {random_number}")
-    else:
-        print(f"Wrong! your guess is {player_guess} and number is {random_number}")
-    turns += 1
+    difficulty_level = input('Please choose a game mode by typing "easy" or "hard": ')
+    turns = game_level(difficulty_level)
+    print(f"You have {game_level(difficulty_level)} turns left" )
+    while turns > 0:
+        player_guess = int(input("Please guess a number between 1 and 100: "))
+        if player_guess < random_number:
+            print("Too low")
+        elif player_guess > random_number:
+            print("Too High")
+        elif player_guess == random_number:
+            print(f"Correct! number is {random_number}")
+            break
+        else:
+            print(f"Wrong! your guess is {player_guess} and number is {random_number}")
+        turns -= 1
+        print(f"Turn left: {turns}")
+    print("Game Over! out of turns")
+
+
+
+play_game()
+
+
