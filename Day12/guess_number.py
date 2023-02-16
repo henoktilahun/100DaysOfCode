832bb645800800f8d1b7944555dacef4f584f066
henoktilahun <henok.tilahun1996@gmail.com>
2023-02-15 21:50:17 -0500

    update with offical answer

--- Day12/guess_number.py
+++ Day12/guess_number.py
@@ -1,40 +1,44 @@
 from art import logo
-import random
+from random import randint
 
 
 print(logo)
 
-def game_level(difficulty_level):
+EASY_TURN = 10
+HARD_TURN = 5
+
+def game_level():
+    difficulty_level = input('Please choose a game mode by typing "easy" or "hard": ')
     if difficulty_level == "easy":
-        turns = 10
+        return EASY_TURN
     elif difficulty_level == "hard":
-        turns = 5
-    
-    return turns
+        return HARD_TURN
+
+
+def check_answer(player_guess, random_number, turns):
+    """checks answer against player guess. Returns the number of turns remaining"""
+    if player_guess < random_number:
+        print("Too low")
+        return turns - 1
+    elif player_guess > random_number:
+        print("Too High")
+        return turns - 1
+    else:
+        print(f"Correct! number is {random_number}")
 
 
 def play_game():
-    random_number = random.randint(1,101)
-    difficulty_level = input('Please choose a game mode by typing "easy" or "hard": ')
-    turns = game_level(difficulty_level)
-    print(f"You have {game_level(difficulty_level)} turns left" )
-    while turns > 0:
+    random_number = randint(1,100)
+    print(f"answer is {random_number}")
+    turns = game_level()
+    player_guess = 0
+    while player_guess != random_number:
+        print(f"You have {turns} turns left")
         player_guess = int(input("Please guess a number between 1 and 100: "))
-        if player_guess < random_number:
-            print("Too low")
-        elif player_guess > random_number:
-            print("Too High")
-        elif player_guess == random_number:
-            print(f"Correct! number is {random_number}")
-            break
-        else:
-            print(f"Wrong! your guess is {player_guess} and number is {random_number}")
-        turns -= 1
-        print(f"Turn left: {turns}")
-    print("Game Over! out of turns")
-
-
+        turns = check_answer(player_guess, random_number, turns)
+        if turns == 0:
+            print("You've run out of turns")
+            return
+        
 
 play_game()
-
-
